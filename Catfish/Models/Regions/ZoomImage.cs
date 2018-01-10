572b06ee0f8a0b8be3f9a6ac09f0931300caad42
mruaini <mruaini@ualberta.ca>
2018-01-10 15:53:47 -0700

    add ZoomImage region

---
+++ Catfish/Models/Regions/ZoomImage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Piranha.Extend;
+using System.ComponentModel.Composition;
+using System.ComponentModel.DataAnnotations;
+
+namespace Catfish.Models.Regions
+{
+    [Export(typeof(IExtension))]
+    [ExportMetadata("InternalId", "ZoomImage")]
+    [ExportMetadata("Name", "Zoom Image")]
+    [ExportMetadata("Type", ExtensionType.Region)]
+    [Serializable]
+    public class ZoomImage : CatfishRegion
+    {
+        public string ImageUrl { get; set; }
+    }
+}
